97b657078505e2e17d47d0dfdcec1d001ca1633e
H <pur3miish@me.com>
2021-01-14 21:02:00 +1000

    Initial commit.

---
+++ changelog.md
@@ -0,0 +1,5 @@
+# base58 changelog
+
+## 1.0.0
+
+Initial Release.
---
+++ test/index.test.mjs
@@ -0,0 +1,8 @@
+import { TestDirector } from 'test-director'
+import base58_to_binary from './base58_to_binary.test.mjs'
+import binary_to_base58 from './binary_to_base58.test.mjs'
+
+const tests = new TestDirector()
+base58_to_binary(tests)
+binary_to_base58(tests)
+tests.run()
